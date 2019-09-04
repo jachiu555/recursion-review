1b8a5835e37e90379ffc513ceee07a8888a9ac32
jachiu555 <jachiu555@gmail.com>
2019-09-03 18:49:01 -0700

    Completed getElementsByClassName and changed the FILL_ME_IN on the spec sheet.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,20 +4,24 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className, doc) {
+var getElementsByClassName = function(className) {
   // your code here
+  var body = document.body;                                                           // store document.body as a variable for easier readability
+  var storage = [];                                                                   // empty storage array
 
-  var returnArray = [];
-  if (!doc) {
-    var doc = [...document.body];
+  var getElements = function(element) {                                               // set up an inner function to iterate through all the elements within body
+    for (var i = 0; i < body.length; i++) {                                           // loop through the parent elements within document.body
+      if (element[i].classList !== undefined && element[i].classList.contains(className)) {           // if the element has a class at all AND the element within HAS className parameter
+        storage.push(element);                                                        // simply push that className into the storage array
+      }
+    }
+
+    if (element.hasChildNode) {                                                       // if the element has child nodes,
+      getElements(element[i]);                                                        // then we will recurse through that child node by passing it into the function on line 12
+    }                                                                                   // that way it will loop through multiple levels of elements
   }
 
-  for (var x = 0; x < doc.length; x++) {
-    console.log(doc[x]);
+  getElements(body);                                                                  // invoke inner function by passing in the document.body that will be iterated through
 
-
-  }
-
-
-  // if (className === )
+  return storage;                                                                     // return completed storage array with all className elements
 };
